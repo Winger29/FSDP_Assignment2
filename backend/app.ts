bcdb23837f343666c39ca8909db3460ca0ece892
Kaiwen <acca3473@gmail.com>
2026-02-01 12:59:53 +0800

    tweaked cors origin

--- backend/app.ts
+++ backend/app.ts
@@ -6,7 +6,7 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const app = express();
-app.use(cors({ origin: "https://fsdp-assignment2.vercel.app" }));
+app.use(cors({ origin: "*" }));
 app.use(express.json({ limit: "5mb" }));
 app.use(express.urlencoded({ extended: true }));
 
