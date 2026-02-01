962aa1b2b2534a445ae05342ad2973dd7ef594b7
Kaiwen <acca3473@gmail.com>
2026-02-01 13:12:14 +0800

    added vercel.json to backend

--- backend/uploads/app.ts
+++ backend/uploads/app.ts
@@ -42,6 +42,7 @@ app.use(
       process.env.FRONTEND_URL || "http://localhost:5173",
       "http://localhost:5174",
       "http://localhost:5173",
+      "https://fsdp-assignment2.vercel.app"
 
     ],
     credentials: true,
