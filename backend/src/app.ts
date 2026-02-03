62bda9b135bb6f472ff2c5954146da84531b0817
Kaiwen <acca3473@gmail.com>
2026-02-03 22:56:45 +0800

    updated allowed methods

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -44,7 +44,7 @@ app.use(
   cors({
     origin: ["https://snecked-uncollectively-bridgett.ngrok-free.dev"],
     credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH","OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
